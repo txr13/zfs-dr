e6b04c20e4534fd68963950af0b00acd9b93b804
Lance Hathaway <qhltx@yahoo.com>
2018-10-28 11:19:03 -0700

    Added license message.

--- zfs-dr.sh
+++ zfs-dr.sh
@@ -16,6 +16,21 @@
 ## ZFS filesystems. These are exported as incremental snapshots, based off
 ## the previous monthly, weekly, or daily snapshot (whichever was done
 ## last).
+##
+##     Copyright (C) 2018 Lance Hathaway
+##
+##    This program is free software: you can redistribute it and/or modify
+##    it under the terms of the GNU Lesser General Public License as published by
+##    the Free Software Foundation, either version 3 of the License, or
+##    (at your option) any later version.
+##
+##    This program is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU Lesser General Public License for more details.
+##
+##    You should have received a copy of the GNU Lesser General Public License
+##    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
 ### USER-EDITABLE VARIABLES
