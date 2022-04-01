be12fd6f897cb78f3c170340276d6afbcb52c884
Lance Hathaway <qhltx@yahoo.com>
2022-03-31 17:04:34 -0700

    Add -iter and -md arguments for openssl encryption.

--- zfs-dr.sh
+++ zfs-dr.sh
@@ -162,7 +162,7 @@ compress_archive() {
 encrypt_archive() {
   if [[ "$encrypt_backup" == "true" ]]; then
     if [[ -f "$zfsdr_temp_dir/$current_archive" ]]; then
-      openssl enc -aes-256-ctr -pbkdf2 -in "$zfsdr_temp_dir/$current_archive" -out "$zfsdr_temp_dir/$current_archive.enc" -pass file:"$openssl_enc_pw_file" -salt
+      openssl enc -aes-256-ctr -md sha512 -pbkdf2 -iter 150000 -in "$zfsdr_temp_dir/$current_archive" -out "$zfsdr_temp_dir/$current_archive.enc" -pass file:"$openssl_enc_pw_file" -salt
       if [[ "$delete_intermediate_steps_immediately" == "true" ]]; then
         rm "$zfsdr_temp_dir/$current_archive"
       fi
